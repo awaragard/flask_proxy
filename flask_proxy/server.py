b928c2d7bfd0594852470de68514096c0497fff5
jansseneda-adobe <danimaejanssen@gmail.com>
2020-06-19 00:33:29 -0500

    more

--- flask_proxy/server.py
+++ flask_proxy/server.py
@@ -1,7 +1,6 @@
 import inspect
 import logging
 import threading
-from enum import Enum
 
 import os
 import requests
@@ -22,7 +21,6 @@ config.init_logger(resources.get_resource('logging_config.yml'))
 
 class ProxyServer(threading.Thread):
 
-
     def __init__(self,
                  config_filename=None,
                  base_url=None,
@@ -33,54 +31,44 @@ class ProxyServer(threading.Thread):
                  match_on=None,
                  log_level=None,
                  base_url_dict=None,
-                 mock_response_dict=None, ):
+                 mock_response_dict=None,
+                 **kwargs
+                 ):
 
-        defaults, args = self.build_args_dict(locals())
+        args = self.build_args_dict(locals())
         super().__init__()
 
         options = {}
-        invoc_opts = {}
         if config_filename:
             logging.getLogger().info("Reading proxy configuration from: {}".format(config_filename))
             with open(config_filename, 'r') as f:
                 options.update(yaml.safe_load(f))
 
-        # Now override with named parameters and merge with defaults
         options.update(args)
-        defaults.update(options)
-        options = defaults
+        self.port = options.get('port', 8080)
+        self.base_url = options.get('base_url', None)
+        self.protocol = options.get('protocol', 'https')
+        self.base_url_map = options.get('base_url_dict', {})
+        self.log_level = options.get('log_level', logging.DEBUG)
+        self.mock_responses = options.get('mock_response_dict', {})
+        self.host = '{}://localhost:{}'.format(self.protocol, self.port)
+        self.mode = VCRMode.value_of(options.get('record_mode', 'all'))
+        self.options = {k: v for k, v in vars(self).items() if not k.startswith("_")}
 
-
-        #self.base_url = invoc_opts['base_url'] = base_url or file_opts.get('base_url')
-
-        self.protocol = invoc_opts['protocol'] = protocol or options.get('protocol', 'https')
-        self.port = invoc_opts['port'] = port or options.get('port', 8080)
-        self.log_level = invoc_opts['log_level'] = log_level or options.get('log_level', logging.DEBUG)
-        self.base_url_dict = invoc_opts['base_url_dict'] = base_url_dict or options.get('base_url_dict', {})
-        self.mock_response_dict = invoc_opts['mock_response_dict'] = \
-            mock_response_dict or options.get('mock_response_dict', {})
-
-        self.host = invoc_opts['host'] = '{}://localhost:{}'.format(self.protocol, self.port)
-        self.daemon = True
-        self.invoc_opts = invoc_opts
-
-        record_mode = invoc_opts['record_mode'] = record_mode or options.get('record_mode', 'all')
-        cassette_dir = invoc_opts['cassette_dir'] = cassette_dir or options.get('cassette_dir', 'cassettes')
-        match_on = invoc_opts['match_on'] = match_on or options.get('match_on') or ['uri', 'method', 'raw_body']
-        self.mode = VCRMode.value_of(record_mode)
         self.vcr = vcr.VCR(
             record_mode=self.mode.value,
-            cassette_library_dir=cassette_dir,
-            match_on=match_on,
+            cassette_library_dir=options.get('cassette_dir', 'cassettes'),
+            match_on=options.get('match_on', ['uri', 'method', 'raw_body']),
             decode_compressed_response=True
         )
 
         logging.getLogger().setLevel(self.log_level)
         self.logger = view.logger = logging.getLogger("proxy")
-        self.logger.info("Initialized with options: {}".format(invoc_opts))
+        self.logger.info("Initialized with options: {}".format(self.options))
+
         view.proxy_server = self
         error.register(view)
-
+        self.daemon = True
         self.application = Flask(self.name)
         self.application.url_map.strict_slashes = False
         self.application.config.update(os.environ)
@@ -89,12 +77,10 @@ class ProxyServer(threading.Thread):
     def build_args_dict(self, all_locals):
         named_args = list(inspect.signature(self.__init__).parameters.values())
         non_default_args = {}
-        default_args = {}
         for v in named_args:
-            default_args[v.name] = v.default
             if v.name in all_locals and all_locals[v.name] != v.default:
                 non_default_args[v.name] = all_locals[v.name]
-        return default_args, non_default_args
+        return non_default_args
 
     def set_mode(self, mode):
         if not isinstance(mode, VCRMode):
@@ -133,5 +119,3 @@ class ProxyServer(threading.Thread):
             return True
         except ConnectTimeout:
             return False
-
-

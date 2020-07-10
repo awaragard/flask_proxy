403e25f8cb411542bfa3cc4e1806c68bc997c019
jansseneda-adobe <danimaejanssen@gmail.com>
2020-07-10 12:32:52 -0500

    validate

--- test_env/test_process.py
+++ test_env/test_process.py
@@ -4,55 +4,39 @@ import sys
 import os
 import pytest
 
+
 from test_env import init_logger
-from test_env.worker import TestSync
+from test_env.test_sync import TestSync
 
 init_logger(logging.DEBUG)
 logger = logging.getLogger()
 
 root_dir = os.getcwd()
 
+record_mode = False
+check_results = True
+
 @pytest.fixture
-def meta(tmpdir):
+def meta():
     yield
     os.chdir(root_dir)
 
 
-
 def run(name, dir):
-    log_test_info(name)
-    ts = TestSync(name, dir)
-    ts.run_test()
+    test = TestSync(name, dir)
+    test.run_sync()
+
+    if check_results and not record_mode:
+        test.validate_results()
+
 
 class TestUST(object):
 
     def test_all_users(self, meta, tmpdir):
-        run(get_name(), "C:\\ZZ")
+        run(get_name(), tmpdir)
 
-
-def clean_console():
-    pass
-
+    def test_all_users2(self, meta, tmpdir):
+        run(get_name(), tmpdir)
 
 def get_name():
     return sys._getframe(1).f_code.co_name
-
-
-def log_test_info(test_name):
-    divider = "=========================="
-    header = divider + " {0} " + divider
-    logger.info("\n" + header.format("Running test: " + test_name) + "\n")
-
-
-def test_cleanup():
-    clean_console()
-
-# @pytest.fixture()
-# def runner(tmpdir):
-#     if not default_options['test_mode']:
-#         clean_console()
-#     default_options['test_dir'] = tmpdir
-#     yield TestRunner(**default_options)
-#     shutil.rmtree(tmpdir)
-
-
---
+++ test_env/test_sync.py
@@ -0,0 +1,88 @@
+import logging
+from subprocess import Popen, PIPE, STDOUT
+
+import os
+import re
+import shutil
+import yaml
+from distutils.dir_util import copy_tree
+from os.path import dirname, join
+from pytest import fail
+
+from test_env.resources import get_ust_exe
+from test_env.test_cases import get_test_def
+
+# These are errors we do not care about in a normal sync, so will
+# always be part of our skipped errors
+default_skipped_errors = [
+    'success, error',
+    'UMAPI timeout',
+    'Next retry in'
+]
+
+
+class TestSync():
+
+    def __init__(self, name, test_dir):
+        test_def = get_test_def(name)
+        self.test_name = name
+        self.source_dir = dirname(test_def)
+        self.test_dir = str(test_dir)
+        self.logger = logging.getLogger()
+        self.sync_logger = logging.getLogger("test")
+        self.sync_results = []
+
+        with open(test_def) as file:
+            cfg = yaml.safe_load(file)
+            self.sync_args = cfg.get('sync_args', '')
+            self.assertions = cfg.get('assertions', '').splitlines()
+            self.root_config = cfg.get('root_config', 'user-sync-config.yml')
+            self.allowed_errors = cfg.get('allowed_errors') or []
+            self.allowed_errors.extend(default_skipped_errors)
+
+    def run_sync(self):
+        self.log_start()
+
+        # prepare test directory
+        copy_tree(self.source_dir, self.test_dir)
+        shutil.copy(get_ust_exe(), join(self.test_dir, 'ust'))
+
+        # Use CHDIR intentionally so the UST can resolve relative paths for CSV, etc
+        # Absolute paths preferred, but this will not work in every case without updating config for test dir
+        os.chdir(self.test_dir)
+        command = "./ust -c {0} {1}".format(self.root_config, self.sync_args)
+        p = Popen(command.split(" "), stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        for line in iter(p.stdout.readline, b''):
+            line = self.normalize(line)
+            self.sync_logger.info(line)
+            self.sync_results.append(line)
+
+    def validate_results(self):
+        """
+        Check first for actual sync errors, and then verify that our asserted lines are present in sync.
+        :return:
+        """
+        for i, l in enumerate(self.sync_results):
+            if re.search('(ERROR)|(CRITICAL)|(EXCEPTION)|(WARNING)', l, flags=re.IGNORECASE):
+                if not True in {self.normalize(s) in self.normalize(l) for s in self.allowed_errors}:
+                    fail(l)
+
+        fulltext = "".join(self.sync_results).lower()
+        for a in self.assertions:
+            if self.normalize(a) not in fulltext:
+                fail(a + ": not in results")
+
+    def log_start(self):
+        self.logger.info(
+            "\n========================== Begin test: [{}] =========================="
+                .format(self.test_name))
+        self.logger.info("Using: " + os.path.abspath(self.root_config))
+        self.logger.info("CLI args: " + self.sync_args)
+        self.logger.info("Assertions: \n" + "\n".join(self.assertions))
+
+    def normalize(self, text):
+        try:
+            text = text.decode()
+        except:
+            pass
+        return str(text).strip().lower()
